264040b4b0b5d17e7f9dd63726d10fc48c41a307
Ruslanwww <oruslano51@gmail.com>
2018-07-08 02:03:13 +0300

    Add orders input

--- public/app.js
+++ public/app.js
@@ -15,7 +15,8 @@ function add_to_cart(id){
 	var x = window.localStorage.getItem(key);
 	x++;
 	window.localStorage.setItem(key, x);
-	alert('Items in your cart: ' + cart_get_number_of_items());
+
+	update_orders_input();
 }
 
 function cart_get_number_of_items(){
@@ -32,4 +33,26 @@ function cart_get_number_of_items(){
 		}
 	}
 	return cnt;
+}
+
+function update_orders_input(){
+
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+function cart_get_orders(){
+
+	var orders = '';
+	for (var i = 0; i < window.localStorage.length; i++) {
+
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key) * 1;
+
+		if (key.indexOf('product_') == 0) {
+
+			orders += key + '=' + value + ',';
+		}
+	}
+	return orders;
 }
