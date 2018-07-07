faa0b8245a6af6f8ed94e66f4683c845982566c8
Ruslanwww <oruslano51@gmail.com>
2018-07-08 02:25:03 +0300

    Add js function update_orders_button

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
+$(function(){
+	update_orders_input();
+	update_orders_button();
+});
+
 function something(){
 
 	var x = window.localStorage.getItem('bbb');
@@ -17,6 +22,7 @@ function add_to_cart(id){
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+	update_orders_button();
 }
 
 function cart_get_number_of_items(){
@@ -39,6 +45,11 @@ function update_orders_input(){
 
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button(){
+	var text = 'Cart(' + cart_get_number_of_items() + ')'
+	$('#orders_button').val(text);
 }
 
 function cart_get_orders(){
