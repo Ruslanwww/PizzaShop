bd1205d696513dd607be559924fcb2ed8cc5dcb3
Ruslanwww <oruslano51@gmail.com>
2018-07-08 01:12:07 +0300

    Add js function cart_get_number_of_items

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 function something(){
+
 	var x = window.localStorage.getItem('bbb');
 
 	x++;
@@ -9,10 +10,26 @@ function something(){
 }
 
 function add_to_cart(id){
-	var key = 'product_' + id;
-	
 
+	var key = 'product_' + id;
 	var x = window.localStorage.getItem(key);
 	x++;
 	window.localStorage.setItem(key, x);
+	alert('Items in your cart: ' + cart_get_number_of_items());
+}
+
+function cart_get_number_of_items(){
+
+	var cnt = 0;
+	for (var i = 0; i < window.localStorage.length; i++) {
+
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key) * 1;
+
+		if (key.indexOf('product_') == 0) {
+
+			cnt += value;
+		}
+	}
+	return cnt;
 }
