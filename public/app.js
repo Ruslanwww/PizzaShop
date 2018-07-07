87cc42858d5eecf266196bd2f1fc8bcdfe5e6d7c
Ruslanwww <oruslano51@gmail.com>
2018-07-08 00:31:40 +0300

    Add js function add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
-function something()
-{
+function something(){
 	var x = window.localStorage.getItem('bbb');
 
 	x++;
@@ -7,4 +6,13 @@ function something()
 	window.localStorage.setItem('bbb', x);
 
 	alert(x);
+}
+
+function add_to_cart(id){
+	var key = 'product_' + id;
+	
+
+	var x = window.localStorage.getItem(key);
+	x++;
+	window.localStorage.setItem(key, x);
 }
