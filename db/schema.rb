a32c251e735c6f1371cdcc529671a344d0403601
Ruslanwww <oruslano51@gmail.com>
2018-07-09 12:44:01 +0300

    Add migration(add_orders)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_140002) do
+ActiveRecord::Schema.define(version: 2018_07_09_080732) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text "orders_input"
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -21,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_07_06_140002) do
     t.boolean "is_veg"
     t.boolean "is_best_offer"
     t.string "path_to_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
